dfd8cde43649caeb12f80939211f99d7543c07cb
EvanR <evanrinehart@gmail.com>
2014-08-02 18:40:01 -0500

    remove explicit spawn

--- app.rb
+++ app.rb
@@ -1,10 +1,9 @@
 require 'sinatra'
 
-pid = Process.spawn("bundle exec ruby -Ilib dispatcher.rb", :out => :out, :err => :err)
-puts "SPAWNED #{pid}"
+#pid = Process.spawn("bundle exec ruby -Ilib dispatcher.rb", :out => :out, :err => :err)
+#puts "SPAWNED #{pid}"
 
 get '/' do
-  Process.kill 'ALRM', pid
-  puts "wheres my std out?"
+  #Process.kill 'ALRM', pid
   "hello world"
 end
